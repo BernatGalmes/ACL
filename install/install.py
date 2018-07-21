bb82486c67bf0b005f16206f9b1d3f789ebbc716
Bern <bernat_galmes@hotmail.com>
2018-07-21 11:32:49 +0200

    added basic install script

--- install/install.py
+++ install/install.py
@@ -1,45 +1,94 @@
+import pandas as pd
 from src.DataBase import DataBase
-###
-###
-###
-# example
-###
-###
-###
+
+
+def create_database(database_name):
+    # connect to server
+    db = DataBase(host="localhost", user="root", password="")
+    # db.execute("""DROP DATABASE """ + database_name)
+
+    # create database
+    db.execute("""CREATE DATABASE IF NOT EXISTS """ + database_name)
+
+    # use database
+    db.execute("""USE """ + database_name)
+    #
+    # # # Create database from data/tables.sql
+    db.run_sql_file("./data/tables.sql")
+    # db.db.commit()
+    db.cursor.close()
+    db.db.close()
+
+
+def insert_data():
+    # connect to server
+    db = DataBase(host="localhost", user="root", password="")
+
+    # use database
+    db.execute("""USE """ + database_name)
+
+    add_role = ("INSERT INTO main_roles (name) VALUES (%(name)s)")
+    roles_data = pd.read_csv("./data/roles.csv", sep=";", header=0)
+
+    for index, row in roles_data.iterrows():
+        data_role = {
+            roles_data.columns.values[0]: row[0]
+        }
+        print(data_role)
+        db.cursor.execute(add_role, data_role)
+
+    # Make sure data is committed to the database
+    db.db.commit()
+
+    add_permission = ("INSERT INTO main_permissions (tag, description) VALUES (%(tag)s, %(description)s)")
+    roles_data = pd.read_csv("./data/permissions.csv", sep=";", header=0)
+
+    for index, row in roles_data.iterrows():
+        data_permission = {}
+        for i, attr_name in enumerate(roles_data.columns.values):
+            data_permission[attr_name] = row[i]
+
+        print(data_permission)
+        db.cursor.execute(add_permission, data_permission)
+
+    # Make sure data is committed to the database
+    db.db.commit()
+
+    ##
+    # add superuser
+    ##
+    add_superuser = """
+    INSERT INTO users (email, username, password, fname, lname, id_role, email_verified, title)
+    VALUES (%(email)s, %(username)s, %(password)s, %(fname)s, %(lname)s, %(id_role)s, %(email_verified)s, 
+    %(title)s)"""
+
+    superuser_data = {
+        "email": "admin@admin.com",
+        "username": "admin",
+        "password": "$2y$12$jmt4x34D8M5GU7g89PVpzeQGbqkX7AwHjjqsa1FHy2YxurXVgJKuq", # "admin"
+        "fname": "admin",
+        "lname": "admin",
+        "id_role": str(1),
+        "title": "superuser",
+        "email_verified": 1
+    }
+    db.cursor.execute(add_superuser, superuser_data)
+    db.db.commit()
+
+    db.cursor.close()
+    db.db.close()
+
 
 database_name = "testdb"
-
 # connect to server
 db = DataBase(host="localhost", user="root", password="")
-
-# create database
-db.execute("""CREATE DATABASE IF NOT EXISTS """ + database_name)
-
-
-# use database
-db.execute("""USE""" + database_name)
-
-# TODO: create database from data/tables.sql
-
-# TODO: set initial data from data/data.sql
-
-# add superuser
-user_mail = ""
-user_username = ""
-user_pass = ""
-user_pass_hash = ""
-user_fname = ""
-user_lname = ""
-user_permission_id = str(0)
-user_permission_title = "superuser"
-query = """
-INSERT INTO `users` (`email`, `username`, `password`, `fname`, `lname`, `permission_id`, `email_verified`, `title`) VALUES
-(""" + user_mail + """, 
-""" + user_username + """, 
-""" + user_pass_hash + """, 
-""" + user_fname + """, 
-""" + user_lname + """, 
-""" + user_permission_id + """, 1, """ + user_permission_title + """);
-"""
-
+db.execute("""DROP DATABASE """ + database_name)
 db.cursor.close()
+db.db.close()
+
+create_database(database_name)
+
+insert_data()
+
+
+
--- install/src/DataBase.py
+++ install/src/DataBase.py
@@ -1,4 +1,9 @@
+import mysql
 import mysql.connector as conn
+from mysql.connector import errorcode
+
+import datetime, time
+
 
 class DataBase:
     def __init__(self, host="localhost", user="root", password=""):
@@ -6,5 +11,38 @@ class DataBase:
         self.cursor = self.db.cursor()
 
     def execute(self, query):
+        # self.cursor = self.db.cursor()
         self.cursor.execute(query)
-        self.db.commit()
+        # self.cursor.close()
+        # self.db.commit()
+
+    def run_sql_file(self, filename):
+        """
+        The function takes a filename and a connection as input
+        and will run the SQL query on the given connection
+        """
+        start = time.time()
+
+        file = open(filename, 'r')
+        sql = " ".join(file.readlines())
+        print("Start executing: " + filename + " at " + str(datetime.datetime.now().strftime("%Y-%m-%d %H:%M")))
+        try:
+            self.cursor.execute(sql)
+
+        except mysql.connector.Error as err:
+            print(sql)
+            if err.errno == errorcode.ER_TABLE_EXISTS_ERROR:
+                print("already exists.")
+            else:
+                print(err.msg)
+        else:
+            print("OK")
+
+        end = time.time()
+        print("Time elapsed to run the query:")
+        print(str((end - start)*1000) + ' ms')
+        # self.cursor.close()
+
+    def __del__(self):
+        self.cursor.close()
+        self.db.close()
